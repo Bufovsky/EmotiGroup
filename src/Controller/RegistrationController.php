a6eea50be6ff9c5125312353d42c4af0cf26578f
Aleksander <bufostudio@gmail.com>
2023-09-27 23:59:01 +0200

    Add check user exist

--- src/Controller/RegistrationController.php
+++ src/Controller/RegistrationController.php
@@ -1,42 +1,67 @@
 <?php
-  
+
 namespace App\Controller;
-  
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
-use Doctrine\Persistence\ManagerRegistry;
+use Symfony\Component\Validator\Validator\ValidatorInterface;
+use Symfony\Component\Serializer\SerializerInterface;
 use App\Entity\User;
-  
+use App\Repository\UserRepository;
+
 /**
- * @Route("/api", name="api_")
+ * Summary of RegistrationController
  */
-  
+#[Route('/api', name: 'api_')]
 class RegistrationController extends AbstractController
 {
+    #[Route('/register', name: 'register', methods: ['post'])]
     /**
-     * @Route("/register", name="register", methods={"POST"})
+     * Summary of index
+     * @param \Symfony\Component\HttpFoundation\Request $request
+     * @param \Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface $passwordHasher
+     * @param \App\Repository\UserRepository $userRepository
+     * @param \Symfony\Component\Validator\Validator\ValidatorInterface $validator
+     * @param \Symfony\Component\Serializer\SerializerInterface $serializer
+     * @return \Symfony\Component\HttpFoundation\JsonResponse
      */
-    public function index(ManagerRegistry $doctrine, Request $request, UserPasswordHasherInterface $passwordHasher): Response
-    {
-          
-        $em = $doctrine->getManager();
-        $decoded = json_decode($request->getContent());
-        $email = $decoded->email;
-        $plaintextPassword = $decoded->password;
-  
-        $user = new User();
-        $hashedPassword = $passwordHasher->hashPassword(
-            $user,
-            $plaintextPassword
+    public function index(
+        Request $request,
+        UserPasswordHasherInterface $passwordHasher,
+        UserRepository $userRepository,
+        ValidatorInterface $validator,
+        SerializerInterface $serializer
+    ): JsonResponse {
+        $requestJson = json_decode($request->getContent());
+        $requestSerialized = $serializer->serialize($requestJson, 'json');
+        $registrationDto = $serializer->deserialize(
+            $requestSerialized,
+            User::class,
+            'json'
         );
-        $user->setPassword($hashedPassword);
-        $user->setEmail($email);
-        $em->persist($user);
-        $em->flush();
-  
-        return $this->json(['message' => 'Registered Successfully']);
+        $errors = $validator->validate($registrationDto);
+
+        if (\count($errors) > 0) {
+            return new JsonResponse(
+                "$errors",
+                Response::HTTP_BAD_REQUEST
+            );
+        }
+
+        $hashedPassword = $passwordHasher->hashPassword(
+            new User,
+            $requestJson->password
+        );
+        $registrationDto->setPassword($hashedPassword);
+        $userRepository->save($registrationDto, true);
+
+        return new JsonResponse(
+            'User registered successfully.',
+            Response::HTTP_CREATED
+        );
     }
 }
